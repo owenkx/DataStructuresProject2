04e36fe1bd5b7b21e5c8192af73599125c075517
Owen <owen@ubuntu.(none)>
2014-01-22 15:39:51 -0800

    Finished stringcomp

--- CSE332_Project2/src/phaseA/StringComparator.java
+++ CSE332_Project2/src/phaseA/StringComparator.java
@@ -1,7 +1,6 @@
 package phaseA;
 import providedCode.*;
 
-
 /**
  * TODO: Replace this comment with your own as appropriate.
  * This comparator is used by the provided code for both data-counters and 
@@ -12,10 +11,22 @@ import providedCode.*;
  * use are length and charAt.
  */
 public class StringComparator implements Comparator<String>{
-
+	
 	@Override
 	public int compare(String s1, String s2) {
-		// TODO: To-be implemented
-		return 0;
+		int length = s1.length();
+		if (s1.length() > s2.length())
+			length = s2.length();
+		
+		for (int i = 0; i < length; i++) {
+			char char1 = s1.charAt(i);
+			char char2 = s2.charAt(i); 
+			if (char1 < char2)
+				return -1;
+			else if (char2 < char1)
+				return 1;
+		}
+		
+		return s1.length() - s2.length();
 	}
 }
