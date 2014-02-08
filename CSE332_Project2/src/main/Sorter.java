7bf45856ca74d9d5081a29a54f4e6cf3520fef46
Brian Griffith <briangri@jes.cs.washington.edu>
2014-02-07 16:27:35 -0800

    quicksort

--- CSE332_Project2/src/main/Sorter.java
+++ CSE332_Project2/src/main/Sorter.java
@@ -8,42 +8,114 @@ import phaseA.FourHeap;
  *  but you may add as many other methods as you want.
  */
 public class Sorter {
-	
+
 	/**
-     * Sort the count array in descending order of count. If two elements have
-     * the same count, they should be ordered according to the comparator.
-     * This code uses insertion sort. The code is generic, but in this project
-     * we use it with DataCount<String> and DataCountStringComparator.
-     * @param counts array to be sorted.
+	 * Sort the count array in descending order of count. If two elements have
+	 * the same count, they should be ordered according to the comparator.
+	 * This code uses insertion sort. The code is generic, but in this project
+	 * we use it with DataCount<String> and DataCountStringComparator.
+	 * @param counts array to be sorted.
 	 * @param comparator for comparing elements.
-     */
-    public static <E> void insertionSort(E[] array, Comparator<E> comparator) {
-        for (int i = 1; i < array.length; i++) {
-            E x = array[i];
-            int j;
-            for (j=i-1; j>=0; j--) {
-                if (comparator.compare(x,array[j]) >= 0) { break; }
-                array[j + 1] = array[j];
-            }
-            array[j + 1] = x;
-        }
-    }
-    
-    //Takes in an array and comparator, then sorts the array by placing
-    //all of the elements in a heap and then removing them.
-    public static <E> void heapSort(E[] array, Comparator<E> comparator) {
-    	FourHeap<E> heap = new FourHeap<E>(array, comparator, array.length);
-    	for (int i = 0; i < array.length; i++) {
-    		array[i] = heap.deleteMin();
-    	}
-    }
-    
-    public static <E> void topKSort(E[] array, Comparator<E> comparator, int k) {
-    	// TODO: To-be implemented (the order of elements at index >= k does not matter)
-    }
-    
-    public static <E> void otherSort(E[] array, Comparator<E> comparator) {
-    	// TODO: To-be implemented (either mergeSort or QuickSort)
-    }
+	 */
+	public static <E> void insertionSort(E[] array, Comparator<E> comparator) {
+		for (int i = 1; i < array.length; i++) {
+			E x = array[i];
+			int j;
+			for (j=i-1; j>=0; j--) {
+				if (comparator.compare(x,array[j]) >= 0) { break; }
+				array[j + 1] = array[j];
+			}
+			array[j + 1] = x;
+		}
+	}
+
+	//Takes in an array and comparator, then sorts the array by placing
+	//all of the elements in a heap and then removing them.
+	public static <E> void heapSort(E[] array, Comparator<E> comparator) {
+		FourHeap<E> heap = new FourHeap<E>(array, comparator, array.length);
+		for (int i = 0; i < array.length; i++) {
+			array[i] = heap.deleteMin();
+		}
+	}
+
+	public static <E> void topKSort(E[] array, Comparator<E> comparator, int k) {
+		// TODO: To-be implemented (the order of elements at index >= k does not matter)
+	}
+
+	public static <E> void otherSort(E[] array, Comparator<E> comparator) {
+		if (array.length >= 2) { 
+			quickSort(array, 0, array.length - 1, comparator);
+		}
+	}
+
+	private static <E> void quickSort(E[] contents, int left, int right, Comparator<E> comparator) {
+		if ((left + 2) > right) {
+			if (comparator.compare(contents[left], contents[right]) > 0) { swap(contents, left, right); }
+		} else if (left < right) {
+			medianOf3Pivot(contents, left, right, comparator);
+			int pivotIndex = partition(contents, left+1, right-1, comparator);
+			quickSort(contents, left, pivotIndex - 1, comparator);
+			quickSort(contents, pivotIndex + 1, right, comparator);
+		}
+	}
+
+	private static <E> void medianOf3Pivot(E[] contents, int left, int right, Comparator<E> comparator) {
+
+		int middle = (left + right) / 2;
+		int l, r, pivot;
+
+		if (comparator.compare(contents[right], contents[middle]) >= 0 && comparator.compare(contents[right], contents[left]) >= 0) {
+			r = right; 
+			if (comparator.compare(contents[middle], contents[left]) >= 0) { pivot = middle; l = left; }
+			else { pivot = left; l = middle; }
+		} else if (comparator.compare(contents[left], contents[middle]) >= 0 && comparator.compare(contents[left], contents[right]) >= 0) {
+			r = left; 
+			if (comparator.compare(contents[middle], contents[right]) >= 0) { pivot = middle; l = right; }
+			else { pivot = right; l = middle; }
+		} else { // middle is the largest
+			r = middle;
+			if (comparator.compare(contents[left], contents[right]) >= 0) { pivot = left; l = right; }
+			else { pivot = right; l = left; }
+		}
+
+		E temp_l = contents[l];
+		E temp_r = contents[r];
+		E temp_pivot = contents[pivot];
+		contents[middle] = contents[right - 1];
+		contents[right-1] = temp_pivot;
+		contents[left] = temp_l;
+		contents[right] = temp_r;
+
+	}
+
+	private static <E> int partition(E[] contents, int left, int right, Comparator<E> comparator) {
+
+		// assume pivot value is at right
+		E v = contents[right];
+		int i = left;
+		int j = right - 1;
+
+		while (true) {
+			while (comparator.compare(contents[i], v) < 0) { i++; }
+			while (comparator.compare(contents[j], v) > 0) { j--; }
+			if (i < j) {
+				swap(contents, i, j);
+				i++;
+				j--;
+			} else { 
+				break;
+			}
+		}
+
+		swap(contents, i, right);
+
+		return i;
+	}
+
+	public static <E> void swap(E[] arr, int i, int j) {
+		E temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
 
 }
