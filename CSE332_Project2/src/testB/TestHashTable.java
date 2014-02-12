3d11bd394d9eef3a0ef6f44963c0b80144d036bd
Owen <owen@ubuntu.(none)>
2014-02-11 16:42:33 -0800

    Finished hash table tests

--- CSE332_Project2/src/testB/TestHashTable.java
+++ CSE332_Project2/src/testB/TestHashTable.java
@@ -27,7 +27,7 @@ public class TestHashTable extends TestDataCounter<Integer>{
 						}
 					}, new Hasher<Integer>() {
 						public int hash(Integer data) {
-							return Math.abs(data * 7 << 1);
+							return Math.abs(data * 17 );
 						}
 					});
 	}
@@ -37,47 +37,77 @@ public class TestHashTable extends TestDataCounter<Integer>{
 		assertTrue(dc.getSize() == 0);
 	}
 	
-	@Test (timeout = TIMEOUT, expected = java.util.NoSuchElementException.class)
-	public void testNoSuchElementException() {
-		dc.getCount(0);
+	@Test (timeout = TIMEOUT)
+	public void testGetCount() {
+		assertEquals(dc.getCount(0), 0);
 	}
 
 	@Test (timeout = TIMEOUT)
 	public void testSimpleHash() {
-		dc.incCount(0);
-		assertEquals(dc.getCount(0), 1);
+		Integer[] testArray = {0};
+		addAndConfirm(testArray, 0, 1);
 	}
 	
 	@Test (timeout = TIMEOUT)
 	public void testConsistentMapping() {
-		dc.incCount(0);
-		dc.incCount(0);
-		assertEquals(dc.getCount(0), 2);
+		Integer[] testArray = {0, 0};
+		addAndConfirm(testArray, 0, 2);
 	}
 	
 	@Test (timeout = TIMEOUT)
-	public void testTwoValues() {
-		dc.incCount(0);
-		dc.incCount(1);
+	public void testTwoInserts() {
+		addAndConfirm(new Integer[]{0}, 0, 1);
+		addAndConfirm(new Integer[]{1}, 1, 1);
 	}
 	
 	@Test (timeout = TIMEOUT)
-	public void testMultiple() {
-		
+	public void testMultipleInserts() {
+		Integer[] testArray = {1, 2, 3, 4, 5};
+		addAndConfirm(testArray, 1, 1);
+		addAndConfirm(testArray, 2, 2);
+		addAndConfirm(testArray, 3, 3);
+		addAndConfirm(testArray, 4, 4);
+		addAndConfirm(testArray, 5, 5);
 	}
 	
 	@Test (timeout = TIMEOUT)
 	public void testMultipleDuplicates() {
-		
+		Integer[] testArray = {1, 1, 2, 2, 3, 3};
+		addAndConfirm(testArray, 1, 2);
+		addAndConfirm(testArray, 2, 4);
+		addAndConfirm(testArray, 3, 6);
 	}
 	
 	@Test (timeout = TIMEOUT)
 	public void testRehashSize() {
+		Integer[] zero = {0};
+		addAndConfirm(zero, 0, 1);
 		
+		Integer[] testArray = new Integer[100];
+		for (int i = 0; i < 100; i++)
+			testArray[i] = i;
+		addAndConfirm(testArray, 0, 2);
 	}
 	
 	@Test (timeout = TIMEOUT)
 	public void testRehashConsistency() {
+		Integer[] zero = {0};
+		addAndConfirm(zero, 0, 1);
+		Integer[] one = {1};
+		addAndConfirm(one, 1, 1);
 		
+		Integer[] testArray = new Integer[100];
+		for (int i = 0; i < 100; i++)
+			testArray[i] = i;
+		addAndConfirm(testArray, 0, 2);
+		addAndConfirm(testArray, 1, 3);
+		
+	}
+	
+	private void addAndConfirm(Integer[] insertArray, Integer value, int count ) {
+		for (Integer i: insertArray) {
+			dc.incCount(i);
+		}
+		assertEquals(dc.getCount(value), count);
 	}
 }
